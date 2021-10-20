8ce9f9cf8e81354d29cf7e023749cdcaf6516d5d
femialiu <femialiu713@gmail.com>
2021-10-20 21:14:26 +0100

    created custom UIButton and UITextField

---
+++ Devotional/IGButton.swift
@@ -0,0 +1,35 @@
+//
+//  IGButton.swift
+//  Devotional
+//
+//  Created by Femi Aliu on 20/10/2021.
+//
+
+import UIKit
+
+class IGButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
---
+++ Devotional/IGTextField.swift
@@ -0,0 +1,41 @@
+//
+//  IGTextField.swift
+//  Devotional
+//
+//  Created by Femi Aliu on 20/10/2021.
+//
+
+import UIKit
+
+class IGTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .left
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        
+        placeholder = "Search for devotional"
+    }
+
+}
